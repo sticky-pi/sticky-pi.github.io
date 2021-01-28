9362af0a783220154631bf75f8d3fed22daf5b2c
Quentin Geissman <qgeissmann@gmail.com>
2021-01-28 19:21:30 +0200

    doc++

--- _build.sh
+++ _build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', 'bookdown::pdf_book'))"
+Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook'))"
+# Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::pdf_book'))"
