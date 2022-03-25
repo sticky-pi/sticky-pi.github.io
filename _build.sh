e6e97095ee80ce2832746aff0f2f2d28a5e604d9
Quentin Geissman <qgeissmann@gmail.com>
2022-03-25 11:07:37 +0100

    naming of index file fixed

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', 'bookdown::pdf_book'))"
-Rscript -e "bookdown::render_book('00-index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #rsync assets/*.mp4  _book/assets/ -v
 # Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::pdf_book'))"
