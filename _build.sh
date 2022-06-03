3f441ae93999adef1dd4b7a1d54720bc6adc8c53
Quentin Geissman <qgeissmann@gmail.com>
2022-06-03 11:20:52 -0700

    force copy of doc assets to the compiled webpage

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', 'bookdown::pdf_book'))"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')" &&
+rsync -rvP assets/hardware/ _book/assets/hardware/
 #rsync assets/*.mp4  _book/assets/ -v
 # Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::pdf_book'))"
--- global_variables.R
+++ global_variables.R
@@ -1,1 +1,2 @@
 PUBLICATION_URL="https://www.biorxiv.org/content/10.1101/2021.08.11.455750"
+SD_IMAGE_LINK="https://todo"
